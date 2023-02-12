8e5e7564ada289cff0fd51dc2b0a8bdd343f24f8
roko123477 <78839525+roko123477@users.noreply.github.com>
2023-02-12 12:14:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header className="p-4">
+        <a href="" className="flex items-center gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8 -rotate-90"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+            />
+          </svg>
+          <span className="font-bold text-xl">airbnc</span>
+        </a>
+        <div className="flex">
+          <div>Anywhere</div>
+          <div>Any week</div>
+          <div>Any guests</div>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
